82d7e14219b12a241ea2c798dae311b591066ec9
Cave <20140539+cxve@users.noreply.github.com>
2024-02-20 16:00:20 +0100

    Feat: Update start page, add missing localization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+'use client';
+
 import { ButtonLoginSpotify, ButtonSubmitHistory } from "@/components/button";
 import { DividerHorizontal } from "@/components/divider";
+import { Footer } from "@/components/footer";
+import { Lang, lang } from "@/components/lang";
 
 export default function Home() {
-  return (
+  return <>
     <main className="flex min-h-screen flex-col items-center p-24">
       <ButtonSubmitHistory />
       <DividerHorizontal>
-        or
+        <Lang code="start.or" />
       </DividerHorizontal>
       <ButtonLoginSpotify />
     </main>
-  );
+    <Footer />
+  </>
 }
