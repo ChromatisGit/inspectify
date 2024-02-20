4936d4ecb4afd0530fa79cdcc38fe25440221894
Cave <20140539+cxve@users.noreply.github.com>
2024-02-20 14:13:43 +0100

    Feat: Added InternalStateProvider

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 
 const IsClientCtx = createContext(false);
 
@@ -14,3 +14,30 @@ export const IsClientCtxProvider = ({ children }: any) => {
 export function useIsClient() {
   return useContext(IsClientCtx);
 }
+
+interface IState {
+  lang: string;
+}
+
+const initialState: IState = {
+  lang: "en"
+}
+
+const __internalState = createContext(initialState);
+const __internalDispatch = createContext(undefined);
+
+export const InternalStateProvider = ({ children }: any) => {
+    const [state, dispatch] = useReducer((state: IState, newValue: IState) => ({ ...state, ...newValue }), initialState);
+    return <__internalState.Provider value={state}>
+      {/*no idea how to make this work with typescript*/}
+      {/*@ts-ignore*/}
+        <__internalDispatch.Provider value={dispatch}>
+            {children}
+        </__internalDispatch.Provider>
+    </__internalState.Provider>
+}
+
+export const useInternalState = () => [
+    useContext(__internalState),
+    useContext(__internalDispatch)
+];
