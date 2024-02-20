a744856a4c56aa83b8e0e4252441adc5df1cabe0
Cave <20140539+cxve@users.noreply.github.com>
2024-02-20 15:59:27 +0100

    Feat: Add DividerVertical and the ability to set divider color

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,13 +1,21 @@
 import '../styles/divider.css';
 
+interface IDivider {
+    color?: "white";
+}
+
 interface IDividerHorizontal {
     children?: any;
 }
 
-export function DividerHorizontal({ children }: IDividerHorizontal) {
+export function DividerHorizontal({ children, color = 'white' }: IDividerHorizontal & IDivider) {
     return (
-        <div className="divider divider-horizontal">
+        <div className={`divider divider-horizontal divider-${color}`}>
             <div>{children}</div>
         </div>
     )
+}
+
+export function DividerVertical() {
+    return <div className="divider divider-vertical" />
 }
